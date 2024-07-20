03fd4d145ff49861f496890f3a736753ca362e84
ddelange <14880945+ddelange@users.noreply.github.com>
2024-07-20 13:59:24 +0300

    Update 04-run-test-suite.sh

--- ci/04-run-test-suite.sh
+++ ci/04-run-test-suite.sh
@@ -7,12 +7,12 @@ elif [ -f ${HOME}/.bash_profile ]; then
     source ${HOME}/.bash_profile
 fi
 export VAEX_SERVER_OVERRIDE='{"dataframe.vaex.io":"dataframe-dev.vaex.io"}'
-python -m pytest --pyargs --doctest-modules --timeout=1000
-        ./tests\
-        vaex.datatype_test\
-        vaex.file\
-        vaex.test.dataset::TestDataset\
-        vaex.datatype\
-        vaex.utils\
-        vaex.struct\
-        vaex.groupby\
+python -m pytest --doctest-modules --timeout=1000
+        tests\
+        packages/vaex-core/vaex/datatype_test.py\
+        packages/vaex-core/vaex/file/\
+        packages/vaex-core/vaex/test/dataset.py::TestDataset\
+        packages/vaex-core/vaex/datatype.py\
+        packages/vaex-core/vaex/utils.py\
+        packages/vaex-core/vaex/struct.py\
+        packages/vaex-core/vaex/groupby.py\
